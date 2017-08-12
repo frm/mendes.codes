072dbb628a9ff8dd595048d2f7b1705758b5284e
Fernando Mendes <devfrmendes@gmail.com>
2017-08-12 17:27:16 +0100

    Add desktop nav

--- config.rb
+++ config.rb
@@ -15,6 +15,7 @@ end
 page "/*.xml", layout: false
 page "/*.json", layout: false
 page "/*.txt", layout: false
+page "/", layout: "landing"
 
 # With alternative layout
 # page '/path/to/file.html', layout: 'other_layout'
