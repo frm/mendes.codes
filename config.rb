284c0fb2c0f8426054a2b690f4023b174af251eb
Fernando Mendes <devfrmendes@gmail.com>
2017-08-13 16:46:24 +0100

    Fix assets in production

--- config.rb
+++ config.rb
@@ -40,7 +40,7 @@ configure :build do
   # end
 
   # Use relative URLs
-  # activate :relative_assets
+  activate :relative_assets
 
   # Or use a different image path
   # set :http_prefix, "/Content/images/"
